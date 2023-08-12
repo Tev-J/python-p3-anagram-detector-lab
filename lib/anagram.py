b814cca122b6c57a8a448a5b39c711aba1c53215
Tev-J <tj_delph@yahoo.com>
2023-08-12 05:01:11 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.letters = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        match_list = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.letters:
+                match_list.append(word)
+
+        return match_list
